953828f88922e8b2abbef373fae7049388a64bfb
Jakson Kallio <jakson@protonmail.com>
2017-04-11 10:01:54 -0500

    Configuration defines

---
+++ altcart/config.php
@@ -0,0 +1,7 @@
+<?
+define('SHOP_NAME', "My Altcart Shop"); // Name of your shop
+define('EXCLUSIVE_CRYPTO', true); // Only one cryptocurrency used?
+define('PREFERRED_CRYPTO', 0); // Index of the crypto in cryptos.json
+define('LOCAL_FIAT_TICKER', "USD");
+define('CART_EXPIRE', 7776000); // How long before carts expire in seconds
+?>
