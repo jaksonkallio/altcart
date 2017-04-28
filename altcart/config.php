7acc6026f9fc67b0bd3403b289529fc97c6b665e
Jakson Kallio <jakson@protonmail.com>
2017-04-28 12:25:31 -0500

    Main currency set to fiat constant

--- altcart/config.php
+++ altcart/config.php
@@ -1,6 +1,7 @@
 <?
 define('SHOP_NAME', "My Altcart Shop"); // Name of your shop
 define('EXCLUSIVE_CRYPTO', true); // Only one cryptocurrency used?
+define('MAIN_CURRENCY_FIAT', true); // Generate all cryptocurrency prices based on fiat price
 define('PREFERRED_CRYPTO', 0); // Index of the crypto in cryptos.json
 define('LOCAL_FIAT_TICKER', "USD");
 define('CART_EXPIRE', 7776000); // How long before carts expire in seconds
