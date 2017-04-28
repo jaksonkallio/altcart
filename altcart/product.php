5c78c04b646fdcdb4fd98fcf2708ad1cceeb4b29
Jakson Kallio <jakson@protonmail.com>
2017-04-28 12:25:40 -0500

    Product class items

--- altcart/product.php
+++ altcart/product.php
@@ -1,5 +1,29 @@
 <?
-function createProduct($product_name){
+class Product {
+	private $name;
+	private $price;
+	private $fiat_price;
 
+	public function __construct($product_id) {
+		// Load product from the database
+	}
+
+	// Set name of product
+	public function setName($new_name){
+
+	}
+
+	// Set price of product
+	public function setPrice($new_price){
+		// If MAIN_CURRENCY_FIAT is true, new price is in fiat.
+	}
+
+	private function updateProductInfo(){
+
+	}
+
+	private function findOtherPrice(){
+		// Finds the price of the opposite currency off of the base currency.
+	}
 }
 ?>
